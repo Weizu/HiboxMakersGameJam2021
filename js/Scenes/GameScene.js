1db3038c17339d4db0ef23917ceba1318424d39d
Alexandre Achain (Weizu) <alexandre.achain@gmail.com>
2021-04-10 21:55:13 +0200

    Zoom !

--- js/Scenes/GameScene.js
+++ js/Scenes/GameScene.js
@@ -4,14 +4,29 @@ export default class GameScene extends Phaser.Scene {
   }
  
   preload () {
-    // this.load.setBaseURL('http://labs.phaser.io');
+  
     // load images
-    // this.load.image('sky', 'assets/skies/space3.png');
-    // this.load.image('logo', 'assets/logo.png');
+    this.load.image('mountain', 'assets/mountain.png');
+    this.load.image('character', 'assets/character.png');
   }
  
   create () {
-    // this.add.image(400, 300, 'logo');
+
+    this.add.image(1000, 200, 'mountain');
+    //this.cameras.main.centerOn(100, 100);
+
+    this.cameras.main.setZoom(1);
+
+    this.input.on('wheel', function (pointer, gameObjects, deltaX, deltaY, deltaZ) {
+
+      if(this.cameras.main.zoom >= 0.01) {
+        this.cameras.main.zoom += deltaY * 0.01;
+      }
+      else {
+        this.cameras.main.zoom = 0.01;
+      }
+
+    });
   }
 
   update () {
