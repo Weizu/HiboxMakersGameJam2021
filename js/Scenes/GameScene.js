f063906075027490f11a8aae320c7ad415a005ed
Alexandre Achain (Weizu) <alexandre.achain@gmail.com>
2021-04-11 13:31:51 +0200

    Assets chien et loup

--- js/Scenes/GameScene.js
+++ js/Scenes/GameScene.js
@@ -3,17 +3,43 @@ export default class GameScene extends Phaser.Scene {
     super('Game');
   }
  
+  levels = {
+    level1() {
+        
+
+      if(this.cameras.main.zoom >= 1.5) {
+        this.player.x +=5;
+      }
+      /*if(this.cameras.main.zoom < 1.5) {
+        
+      }*/
+
+    },
+    level2() {
+      
+    },
+    level3() {
+      
+    }
+  }
+
   preload () {
   
     // load images
     this.load.image('mountain', 'assets/mountain.png');
     this.load.image('character', 'assets/character.png');
+
+    
+    this.load.image('wolf', 'assets/wolf.png');
+    this.load.image('dog', 'assets/dog.png');
   }
  
   create () {
 
     this.add.image(1000, 200, 'mountain');
     //this.cameras.main.centerOn(100, 100);
+
+    this.player = this.add.sprite(450, 450, 'character');
 
     this.cameras.main.setZoom(1);
 
@@ -25,11 +51,15 @@ export default class GameScene extends Phaser.Scene {
       else {
         this.cameras.main.zoom = 0.01;
       }
-
+      
     });
   }
 
   update () {
+    
+    level1();
 
   }
+
+
 };
